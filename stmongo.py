31f1552b71d81df1df90a66de53288535f4d59f6
mtuong79 <89337936+mtuong79@users.noreply.github.com>
2022-06-10 15:36:25 +0700

    Create stmongo.py

---
+++ stmongo.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pymongo
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    uri = "mongodb+srv://mtuong:Wellcome2@oucru-it.bcq3ord.mongodb.net/?retryWrites=true&w=majority"
+    return pymongo.MongoClient(uri)
+
+client = init_connection()
+
+# Pull data from the collection.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def get_data():
+    db = client.sample_airbnb
+    items = db.mycollection.find()
+    items = list(items)  # make hashable for st.experimental_memo
+    return items
+
+items = get_data()
+
+# Print results.
+for item in items:
+    st.write(f"{item['name']} has a :{item['beds']}:")
